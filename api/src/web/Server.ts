e30d19d60e36d189429a27edf091dc1918c7121c
AnshuJalan <anshujalan206@gmail.com>
2022-08-24 21:23:27 +0530

    fix: remove cors

--- api/src/web/Server.ts
+++ api/src/web/Server.ts
@@ -1,6 +1,5 @@
 import * as express from "express";
 import { Express } from "express";
-import * as cors from "cors";
 import AnalyticsRouter from "./routes/analytics/Router";
 import VERouter from "./routes/ve/Router";
 import { Dependencies } from "../types";
@@ -9,7 +8,6 @@ export function httpServer(dependencies: Dependencies): Express {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(cors());
   app.use("/analytics", AnalyticsRouter(dependencies));
   app.use("/ve", VERouter(dependencies));
   return app;
