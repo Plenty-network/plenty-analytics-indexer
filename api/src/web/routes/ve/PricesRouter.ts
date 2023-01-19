157aa579f5468ac81297a4869a807c9a58621716
AnshuJalan <anshujalan206@gmail.com>
2023-01-19 17:20:02 +0530

    feat: modify ve route for naan

--- api/src/web/routes/ve/PricesRouter.ts
+++ api/src/web/routes/ve/PricesRouter.ts
@@ -6,17 +6,24 @@ import BigNumber from "bignumber.js";
 function build({ getData, dbClient }: Dependencies): Router {
   const router = Router();
 
-  router.get("/", async (_: Request, res: Response) => {
+  router.get("/:token?", async (req: Request<{ token: string | undefined }>, res: Response) => {
     try {
       // Fetch system wide pools and tokens data
       const data = await getData();
+
+      // Check request params validity
+      if (req.params.token && !data.tokens[req.params.token]) {
+        res.json({ error: "Token does not exist." });
+        return;
+      }
 
       const tc = Math.floor(new Date().getTime() / 1000); // Current timestamp
       const tch = Math.floor(tc / 3600) * 3600; // Current hourly rounded timestamp
 
       // Fetch aggregated token record closest (<=) to supplied timestamp
       async function getClosePriceAggregate(ts: number) {
-        return await dbClient.raw(`
+        if (!req.params.token) {
+          return await dbClient.raw(`
           SELECT t.token, t.close_price
           FROM (
             SELECT MAX(ts) mts, token 
@@ -25,6 +32,17 @@ function build({ getData, dbClient }: Dependencies): Router {
           JOIN token_aggregate_hour t ON
             t.token=r.token AND t.ts=r.mts;
         `);
+        } else {
+          return await dbClient.raw(`
+          SELECT t.token, t.close_price
+          FROM (
+            SELECT MAX(ts) mts
+            FROM token_aggregate_hour WHERE ts<=${ts} AND token='${req.params.token}'
+          ) r
+          JOIN token_aggregate_hour t ON
+            t.ts=r.mts;
+        `);
+        }
       }
 
       // Last aggregated data in the form of { token-symbol: { close-price } }
@@ -33,10 +51,7 @@ function build({ getData, dbClient }: Dependencies): Router {
       const tokens: PriceResponse[] = [];
 
       // Loop through every token in the system
-      for (const token of Object.keys(data.tokens)) {
-        // Retrieve data fields from DB entry
-        const priceCH = parseFloat(lastAggregateCH[token]?.close_price ?? 0);
-
+      for (const token of req.params.token ? [req.params.token] : Object.keys(data.tokens)) {
         tokens.push({
           token,
           price: new BigNumber(lastAggregateCH[token]?.close_price ?? 0).toString(),
